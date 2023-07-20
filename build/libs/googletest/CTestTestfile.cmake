b5d5c82d3d83ee72f8925b3a7930504d2422c440
binariusO1 <54156262+binariusO1@users.noreply.github.com>
2023-07-21 01:05:20 +0200

    Delete build directory

--- build/libs/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/CPP/games/honey_engine/honey_engine/libs/googletest
-# Build directory: D:/Projects/CPP/games/honey_engine/build/libs/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/libs/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/CPP/games/honey_engine/honey_engine/libs/googletest/googlemock
-# Build directory: D:/Projects/CPP/games/honey_engine/build/libs/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
