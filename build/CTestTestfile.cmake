df2d74e6ef77fe10315cbfffb429004c622834e6
binariusO1 <binariusO1@gmail.com>
2023-07-20 21:12:34 +0200

    [init]

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/CPP/games/honey_engine/honey_engine
+# Build directory: D:/Projects/CPP/games/honey_engine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("UT")
+subdirs("libs/googletest")
